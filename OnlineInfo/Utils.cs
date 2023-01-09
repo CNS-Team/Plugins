f96c9b31a5cde986ed4db35590f5b584fa01721b
LaoSparrow <laosparrow@outlook.com>
2023-01-09 19:36:07 +0800

    add OnlineInfo

---
+++ OnlineInfo/Utils.cs
@@ -0,0 +1,68 @@
+﻿using LazyUtils;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.MySql;
+using LinqToDB.DataProvider.SQLite;
+using LinqToDB.DataProvider;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using LinqToDB;
+using Microsoft.Data.Sqlite;
+using TShockAPI;
+using TShockAPI.DB;
+using MySql.Data.MySqlClient;
+
+namespace OnlineInfo
+{
+    internal static class Utils
+    {
+        public static IDataProvider DBProvider => OIConfig.Instance.DBType switch
+        {
+            SqlType.Mysql => MySqlTools.GetDataProvider(),
+            SqlType.Sqlite => SQLiteTools.GetDataProvider("SQLite.Classic"),
+            _ => null,
+        };
+
+        public static string DBConnectionString => OIConfig.Instance.DBType switch
+        {
+            SqlType.Mysql => new MySqlConnectionStringBuilder()
+            {
+                Server = OIConfig.Instance.MySqlHost,
+                Port = OIConfig.Instance.MySqlPort,
+                Database = OIConfig.Instance.MySqlName,
+                UserID = OIConfig.Instance.MySqlUser,
+                Password = OIConfig.Instance.MySqlPassword
+            }.ConnectionString,
+
+            SqlType.Sqlite => new SqliteConnectionStringBuilder()
+            {
+                DataSource = Path.IsPathRooted(OIConfig.Instance.SqlitePath) ? OIConfig.Instance.SqlitePath : Path.Combine(TShock.SavePath, OIConfig.Instance.SqlitePath),
+                Pooling = true
+            }.ConnectionString,
+
+            _ => string.Empty
+        };
+
+        public static DataConnection GetDBConnection() => new DataConnection(DBProvider, DBConnectionString);
+
+        public static DisposableQuery<T> GetDBQuery<T>() where T : class
+        {
+            var dbconn = GetDBConnection();
+            return new DisposableQuery<T>(dbconn.GetTable<T>(), dbconn);
+        }
+    }
+
+    internal static class Logger
+    {
+        private static readonly string AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        public static void ConsoleInfo(string message) =>
+            TShock.Log.ConsoleInfo($"[{AssemblyName}] [INFO] {message}");
+
+        public static void ConsoleError(string message, [CallerMemberName] string callerName = "") =>
+            TShock.Log.ConsoleError($"[{AssemblyName}] [ERROR] [{callerName}] {message}");
+
+        public static void ConsoleDebug(string message, [CallerMemberName] string callerName = "") =>
+            TShock.Log.ConsoleDebug($"[{AssemblyName}] [DEBUG] [{callerName}] {message}");
+    }
+}
