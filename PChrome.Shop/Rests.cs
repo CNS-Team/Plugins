09cf31875602e20e23d9e5040fadeb86b78707e6
cc004 <1176321897@qq.com>
2023-01-09 17:40:14 +0800

    add pchrome

---
+++ PChrome.Shop/Rests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazyUtils;
+using LazyUtils.Commands;
+using Newtonsoft.Json.Linq;
+using Rests;
+using TShockAPI;
+
+namespace PChrome.Shop
+{
+    [Rest("shop")]
+    public static class Rests
+    {
+        [Permissions("economy.shop.player")]
+        public static JToken getshopitems(RestRequestArgs args)
+        {
+            using var context = Db.Context<ShopItem>();
+            return JToken.FromObject(context.Config.OrderByDescending(d => d.id).ToArray());
+        }
+    }
+}
