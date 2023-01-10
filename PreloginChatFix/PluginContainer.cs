7728b781f6c4796cd276a508322ade841f5711a7
cc004 <1176321897@qq.com>
2023-01-10 20:30:05 +0800

    add preloginchatfix

---
+++ PreloginChatFix/PluginContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazyUtils;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace PreloginChatFix
+{
+    [ApiVersion(2, 1)]
+    public class PluginContainer : LazyPlugin
+    {
+        public PluginContainer(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            TShockAPI.Hooks.PlayerHooks.PlayerChat += PlayerHooks_PlayerChat;
+            base.Initialize();
+        }
+
+        private static void PlayerHooks_PlayerChat(TShockAPI.Hooks.PlayerChatEventArgs e)
+        {
+            if (!e.Player.Active && !e.Player.IsLoggedIn)
+                e.Player.Ban("trying to chat before spawn");
+        }
+    }
+}
