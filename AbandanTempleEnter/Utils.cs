1a40eb6acd919c3fdedd40aa90f99b7a46364666
Cai <13110818005@qq.com>
2023-01-11 13:33:38 +0800

    [AbandanTempleEnter]Add AbandanTempleEnter Plugin

---
+++ AbandanTempleEnter/Utils.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using TShockAPI;
+
+
+namespace AbandanTempleEnter
+{
+    internal class Utils
+    {
+        public static Config Config { get; set; } = new();
+
+        public static bool IsInZoneLihzhardTemple(TSPlayer plr)
+        {
+            return Main.player[plr.Index].ZoneLihzhardTemple;
+        }
+
+        public static bool CheckProgress()
+        {
+            if (Config.hardMode)
+            {
+                if (!Main.hardMode)
+                    return false;
+            }
+            if (Config.threeBoss)
+            {
+                if (!NPC.downedMechBoss1 || !NPC.downedMechBoss2 || !NPC.downedMechBoss3)
+                    return false;
+            }
+            if (Config.plantBoss)
+            {
+                if (!NPC.downedPlantBoss)
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
+
