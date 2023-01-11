dfb73115834cc2b34b2eccb1ba0b6ae84cde937a
cc004 <1176321897@qq.com>
2023-01-11 12:03:40 +0800

    add invisibility, preloginchatfix, statustxtmgr

---
+++ Invisibility/Invisibility.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Terraria.GameContent.Creative;
+
+[ApiVersion(2, 1)]
+public class PluginContainer : TerrariaPlugin
+{
+    public override string Name => "Invisibility";
+
+    public override Version Version => Assembly.GetExecutingAssembly().GetName().Version!;
+
+    public override string Author => "1413";
+
+    public PluginContainer(Main game)
+        : base(game)
+    {
+
+    }
+
+    public override void Initialize()
+    {
+        Commands.ChatCommands.Add(new Command("tofout.invisibility.on", cmd => ToGhost(cmd.Player), "invon"));
+        Commands.ChatCommands.Add(new Command("tofout.invisibility.off", cmd => ToActive(cmd.Player), "invoff"));
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        Commands.ChatCommands.RemoveAll(cmd => cmd.HasAlias("invon"));
+        Commands.ChatCommands.RemoveAll(cmd => cmd.HasAlias("invoff"));
+    }
+
+    private void ToGhost(TSPlayer tsplayer)
+    {
+        if (tsplayer == null)
+        {
+            return;
+        }
+        var index = tsplayer.Index;
+        tsplayer.RespawnTimer = int.MaxValue;
+        // IsGhost = true;
+        NetMessage.SendData(14, -1, index, null, index, false.GetHashCode());
+        tsplayer.GodMode = true;
+        var power = CreativePowerManager.Instance.GetPower<CreativePowers.GodmodePower>();
+        power.SetEnabledState(index, true);
+
+        if (false)
+        {
+            NetMessage.SendData(4, -1, index, null, index, 0f, 0f, 0f, 0, 0, 0);
+            NetMessage.SendData(13, -1, index, null, index, 0f, 0f, 0f, 0, 0, 0);
+        }
+    }
+    public void ToActive(TSPlayer tsplayer)
+    {
+        var tplayer = tsplayer.TPlayer;
+        var index = tsplayer.Index;
+        tplayer.active = true;
+        // IsGhost = false;
+        var power = CreativePowerManager.Instance.GetPower<CreativePowers.GodmodePower>();
+        power.SetEnabledState(index, false);
+        tsplayer.GodMode = false;
+        NetMessage.SendData(14, -1, index, null, index, true.GetHashCode());
+
+        if (true)
+        {
+            NetMessage.SendData(4, -1, index, null, index, 0f, 0f, 0f, 0, 0, 0);
+            NetMessage.SendData(13, -1, index, null, index, 0f, 0f, 0f, 0, 0, 0);
+        }
+    }
+}
---
+++ PreloginChatFix/PluginContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazyUtils;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace PreloginChatFix
+{
+    [ApiVersion(2, 1)]
+    public class PluginContainer : LazyPlugin
+    {
+        public PluginContainer(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerChat.Register(this, ServerChat, 9999);
+            base.Initialize();
+        }
+
+        private static void ServerChat(ServerChatEventArgs args)
+        {
+            var plr = TShock.Players[args.Who];
+            if (plr.Account == null)
+                plr.Disconnect("trying to chat before logged in.");
+        }
+    }
+}
