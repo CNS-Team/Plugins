e950292923a6b83a168fd57922eb07bf30fd19b2
cc004 <1176321897@qq.com>
2023-02-01 13:34:30 +0800

    [LazyUtils] fix realPlayer judge for help command

--- MusicPlayer/MidiParser.cs
+++ MusicPlayer/MidiParser.cs
@@ -4,34 +4,36 @@ namespace MusicPlayer;
 
 public static class MidiParser
 {
-    private delegate void ReadMidiCallback(IntPtr mem, ref Midi midi);
-    [DllImport("midiParser.dll")]
-    private static extern void ReadMidi(string filename, ReadMidiCallback callback, ref Midi midi);
-    [DllImport("midiParser.dll")]
+    private delegate void ReadMidiCallback(IntPtr mem, IntPtr nul);
+    [DllImport("midiParser.dll", EntryPoint = "readMidi")]
+    
+    private static extern void ReadMidi(string filename, ReadMidiCallback callback, IntPtr nul);
+    [DllImport("midiParser.dll", EntryPoint = "getUInt")]
     private static extern uint GetUInt(IntPtr mem, int index);
-    [DllImport("midiParser.dll")]
+    [DllImport("midiParser.dll", EntryPoint = "getUShort")]
     private static extern ushort GetUShort(IntPtr mem, int index);
-    [DllImport("midiParser.dll")]
+    [DllImport("midiParser.dll", EntryPoint = "getByte")]
     private static extern byte GetByte(IntPtr mem, int index);
     
-    private static void Callback(IntPtr mem, ref Midi res)
-    {
-        uint size = GetUInt(mem, 0);
-        res.notes = new Note[size];
-        for (int i = 0; i < size; ++i)
-        {
-            res.notes[i].time = GetUInt(mem, 12 * i + 4);
-            res.notes[i].lasting = GetUShort(mem, 12 * i + 8);
-            res.notes[i].instrument = GetByte(mem, 12 * i + 10);
-            res.notes[i].pitch = GetByte(mem, 12 * i + 11);
-            res.notes[i].velocity = GetByte(mem, 12 * i + 12);
-        }
-    }
-
     public static Midi ParseMidi(string filename)
     {
         Midi res = new Midi();
-        ReadMidi(filename, Callback, ref res);
+        
+        void Callback(IntPtr mem, IntPtr nul)
+        {
+            uint size = GetUInt(mem, 0);
+            res.notes = new Note[size];
+            for (int i = 0; i < size; ++i)
+            {
+                res.notes[i].time = GetUInt(mem, 12 * i + 4);
+                res.notes[i].lasting = GetUShort(mem, 12 * i + 8);
+                res.notes[i].instrument = GetByte(mem, 12 * i + 10);
+                res.notes[i].pitch = GetByte(mem, 12 * i + 11);
+                res.notes[i].velocity = GetByte(mem, 12 * i + 12);
+            }
+        }
+        
+        ReadMidi(filename, Callback, IntPtr.Zero);
         return res;
     }
 }
