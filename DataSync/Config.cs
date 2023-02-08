c83ae3ad7ee5609036ce5a0640811fd033303634
SGKoishi <i@sgkoi.dev>
2023-02-08 04:18:44 -0800

    Add more progress NPCID

--- DataSync/Config.cs
+++ DataSync/Config.cs
@@ -38,6 +38,11 @@ public enum ProgressType
     [Alias("肉山", "血肉之墙", "血肉墙")]
     WallOfFlesh,
 
+    [Mapping(nameof(Terraria.NPC.downedMechBossAny), true)]
+    [Match(NPCID.Retinazer, NPCID.Spazmatism, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.SkeletronPrime, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice)]
+    [Alias("任一三王")]
+    MechBoss,
+
     [Mapping(nameof(Terraria.NPC.downedMechBoss1), true)]
     [Match(NPCID.Retinazer, NPCID.Spazmatism)]
     [Alias("双子魔眼", "机械眼")]
@@ -55,7 +60,7 @@ public enum ProgressType
 
     [Mapping(nameof(Terraria.NPC.downedPlantBoss), true)]
     [Match(NPCID.Plantera)]
-    [Alias("世纪之花")]
+    [Alias("世纪之花", "世花")]
     Plantera,
 
     [Mapping(nameof(Terraria.NPC.downedGolemBoss), true)]
@@ -70,32 +75,32 @@ public enum ProgressType
 
     [Mapping(nameof(Terraria.NPC.downedAncientCultist), true)]
     [Match(NPCID.CultistBoss)]
-    [Alias("拜月教徒", "拜月教邪教徒")]
+    [Alias("拜月教徒", "拜月教邪教徒", "教徒")]
     LunaticCultist,
 
     [Mapping(nameof(Terraria.NPC.downedMoonlord), true)]
     [Match(NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye)]
-    [Alias("月球领主")]
+    [Alias("月球领主", "月总")]
     MoonLord,
 
     [Mapping(nameof(Terraria.NPC.downedTowerSolar), true)]
     [Match(NPCID.SolarCrawltipedeHead, NPCID.SolarCrawltipedeBody, NPCID.SolarCrawltipedeTail, NPCID.SolarDrakomire, NPCID.SolarDrakomireRider, NPCID.LunarTowerSolar)]
-    [Alias("日耀塔")]
+    [Alias("日耀塔", "日耀柱")]
     SolarPillar,
 
     [Mapping(nameof(Terraria.NPC.downedTowerVortex), true)]
     [Match(NPCID.VortexHornetQueen, NPCID.VortexHornet, NPCID.VortexLarva, NPCID.VortexRifleman, NPCID.VortexSoldier, NPCID.LunarTowerVortex)]
-    [Alias("星旋塔")]
+    [Alias("星旋塔", "星旋柱")]
     VortexPillar,
 
     [Mapping(nameof(Terraria.NPC.downedTowerNebula), true)]
     [Match(NPCID.NebulaBrain, NPCID.NebulaBeast, NPCID.NebulaHeadcrab, NPCID.NebulaSoldier, NPCID.LunarTowerNebula)]
-    [Alias("星云塔")]
+    [Alias("星云塔", "星云柱")]
     NebulaPillar,
 
     [Mapping(nameof(Terraria.NPC.downedTowerStardust), true)]
     [Match(NPCID.StardustCellBig, NPCID.StardustCellSmall, NPCID.StardustJellyfishBig, NPCID.StardustJellyfishSmall, NPCID.StardustSpiderBig, NPCID.StardustSpiderSmall, NPCID.LunarTowerStardust)]
-    [Alias("星尘塔")]
+    [Alias("星尘塔", "星尘柱")]
     StardustPillar,
 
     [Mapping(nameof(Terraria.NPC.downedChristmasIceQueen), true)]
@@ -135,29 +140,34 @@ public enum ProgressType
 
     #region Invasion
     [Mapping(nameof(Terraria.Main.bloodMoon), true)]
+    [Match(NPCID.TheGroom, NPCID.TheBride, NPCID.BloodZombie, NPCID.Drippler)]
     [Alias("血月")]
     BloodMoon,
+
+    [Match(NPCID.GoblinShark, NPCID.BloodEelBody, NPCID.BloodEelHead, NPCID.BloodEelTail, NPCID.BloodNautilus, NPCID.BloodSquid)]
+    [Alias("困难血月")]
+    BloodMoonHardmode,
 
     [Mapping(nameof(Terraria.Main.eclipse), true)]
     [Match(NPCID.Vampire, NPCID.VampireBat, NPCID.Frankenstein, NPCID.CreatureFromTheDeep, NPCID.Eyezor, NPCID.ThePossessed, NPCID.Fritz, NPCID.SwampThing)]
     [Alias("日蚀")]
     SolarEclipse,
 
-    [Mapping(nameof(Terraria.Main.eclipse), true)]
     [Match(NPCID.Reaper)]
     [Alias("三王后日蚀")]
     SolarEclipseMech,
 
-    [Mapping(nameof(Terraria.Main.eclipse), true)]
     [Match(NPCID.Mothron, NPCID.MothronSpawn, NPCID.Butcher, NPCID.DeadlySphere, NPCID.DrManFly, NPCID.Nailhead, NPCID.Psycho)]
     [Alias("花后日蚀")]
     SolarEclipsePlantera,
 
     [Mapping(nameof(Terraria.Main.pumpkinMoon), true)]
+    [Match(NPCID.Scarecrow1, NPCID.Scarecrow2, NPCID.Scarecrow3, NPCID.Scarecrow4, NPCID.Scarecrow5, NPCID.Scarecrow6, NPCID.Scarecrow7, NPCID.Scarecrow8, NPCID.Scarecrow9, NPCID.Scarecrow10, NPCID.Splinterling, NPCID.Hellhound, NPCID.Poltergeist, NPCID.HeadlessHorseman, NPCID.MourningWood, NPCID.Pumpking, NPCID.PumpkingBlade)]
     [Alias("南瓜月")]
     PumpkinMoon,
 
     [Mapping(nameof(Terraria.Main.snowMoon), true)]
+    [Match(NPCID.PresentMimic, NPCID.Flocko, NPCID.GingerbreadMan, NPCID.ZombieElf, NPCID.ElfArcher, NPCID.ElfCopter, NPCID.Nutcracker, NPCID.Krampus, NPCID.Everscream, NPCID.SantaNK1, NPCID.IceQueen)]
     [Alias("霜月", "雪月")]
     FrostMoon,
 
@@ -185,22 +195,23 @@ public enum ProgressType
     PiratesArmy,
 
     [Mapping(nameof(Terraria.Main.invasionType), 2)]
+    [Match(NPCID.MisterStabby, NPCID.SnowmanGangsta, NPCID.SnowBalla)]
     [Alias("雪人军团")]
     FrostLegion,
 
     [Mapping(nameof(Terraria.GameContent.Events.DD2Event._downedDarkMageT1), true)]
     [Match(NPCID.DD2DarkMageT1, NPCID.DD2DarkMageT3)]
-    [Alias("暗黑法师")]
+    [Alias("一阶旧日军团", "暗黑法师")]
     DD2Mage,
 
     [Mapping(nameof(Terraria.GameContent.Events.DD2Event._downedOgreT2), true)]
     [Match(NPCID.DD2OgreT2, NPCID.DD2OgreT3)]
-    [Alias("巨魔")]
+    [Alias("二阶旧日军团", "巨魔")]
     DD2Orge,
 
     [Mapping(nameof(Terraria.GameContent.Events.DD2Event._spawnedBetsyT3), true)]
     [Match(NPCID.DD2Betsy)]
-    [Alias("贝蒂斯")]
+    [Alias("三阶旧日军团", "贝蒂斯")]
     DD2Betsy,
     #endregion
 
