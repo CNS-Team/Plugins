e6cd0069a0657f54f6dc5892be40a18287fd18f9
GUZZ07 <tofout@qq.com>
2023-02-20 02:19:47 +0800

    添加物品存储模型

---
+++ PiggyChest/ItemInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria;
+
+namespace PiggyChest;
+
+public struct ItemInfo
+{
+    public int ID { get; set; }
+    public short Stack { get; set; }
+    public byte Prefix { get; set; }
+    public ItemInfo(int id, int stack = 0, int prefix = 0)
+    {
+        this.ID = id;
+        checked
+        {
+            this.Stack = (short) stack;
+            this.Prefix = (byte) prefix;
+        }
+    }
+
+    public Item ToItem()
+    {
+        var item = new Item();
+        item.SetDefaults(this.ID);
+        item.stack = this.Stack;
+        item.prefix = this.Prefix;
+        return item;
+    }
+    public override string ToString()
+    {
+        return $"{this.ID}:{this.Stack}:{this.Prefix}";
+    }
+
+    public static implicit operator ItemInfo(Item item) => new ItemInfo(item.type, item.stack, item.prefix);
+    public static implicit operator ItemInfo((int id, int stack, int prefix) tuple) => new ItemInfo(tuple.id, tuple.stack, tuple.prefix);
+    public static implicit operator ItemInfo((int id, int stack) tuple) => new ItemInfo(tuple.id, tuple.stack);
+
+
+    public static ItemInfo Parse(string text)
+    {
+        var values = text.Split(':').Select(s => int.Parse(s)).ToArray();
+        return (values[0], values[1], values[2]);
+    }
+    public static List<ItemInfo> ParseList(string text)
+    {
+        return text
+            .Split('\n')
+            .Select(s => Parse(s))
+            .ToList();
+    }
+}
---
+++ PiggyChest/StorageManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyChest;
+
+public class StorageManager
+{
+    public string StoragePath { get; }
+    public StorageManager(Config config)
+    {
+        this.StoragePath = config.StoragePath;
+        this.StoragePath.EnsureDirectoryExists();
+    }
+    public List<ItemInfo> GetBankItems(int userID, string chestName)
+    {
+        var userPath = Path.Combine(this.StoragePath, userID.ToString());
+        userPath.EnsureDirectoryExists();
+        var chestPath = Path.Combine(userPath, chestName + ".txt");
+        if (!File.Exists(chestPath))
+        {
+            File.WriteAllText(chestPath, "");
+            return new List<ItemInfo>();
+        }
+        else
+        {
+            var text = File.ReadAllText(chestPath);
+            return ItemInfo.ParseList(text);
+        }
+    }
+    public void SaveBankItems(int userID, string chestName, List<ItemInfo> items)
+    {
+        var userPath = Path.Combine(this.StoragePath, userID.ToString());
+        userPath.EnsureDirectoryExists();
+        var chestPath = Path.Combine(userPath, chestName + ".txt");
+        File.WriteAllText(chestPath, items.ToText());
+    }
+}
