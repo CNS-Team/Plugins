5b31b7f789401a84f9d7ca60511d852e4e48d854
dalaoshus <523321293@qq.com>
2023-04-15 18:19:20 +0800

    add Plugin: ServerTool , KeyChanger

---
+++ KeyChanger/KeyTypes.cs
@@ -0,0 +1,12 @@
+namespace KeyChanger;
+
+public enum KeyTypes
+{
+    Temple = 1141,
+    Jungle = 1533,
+    Corruption = 1534,
+    Crimson = 1535,
+    Hallowed = 1536,
+    Frozen = 1537,
+    Desert = 4714
+}
